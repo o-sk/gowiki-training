d53bab5d646a980d538013762a42d096c4464d57
o-sk <m.ohsuka@gmail.com>
2018-10-30 23:48:40 +0900

    Define loadPage method

--- wiki.go
+++ wiki.go
@@ -13,3 +13,9 @@ func (p *Page) save() error {
 	filename := p.Title + ".txt"
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
+
+func loadPage(title string) *Page {
+	filename := title + ".txt"
+	body, _ := ioutil.ReadFile(filename)
+	return &Page{Title: title, Body: body}
+}
