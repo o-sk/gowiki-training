436c807b9b3f8f266cf071d15cd483e24a7ff100
o-sk <m.ohsuka@gmail.com>
2018-11-03 09:03:57 +0900

    Add error handling in saveHandler

--- wiki.go
+++ wiki.go
@@ -59,7 +59,10 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/save/"):]
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
-	p.save()
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
